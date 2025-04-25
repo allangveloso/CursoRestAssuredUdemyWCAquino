0d4538d6411fd509f290bfaffa1ac3fd34f9e237
Allan Veloso <allangveloso@gmail.com>
2025-04-24 21:56:22 -0300

    Adicionando arquivos inciais ao projeto. Junit v4.12

---
+++ src/test/java/app/HelloWorldJUnit.java
@@ -0,0 +1,56 @@
+//*******************************
+//VERSÃO UTILIZADA JUNIT 4.12
+//*******************************
+
+
+package app;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldJUnit {
+
+    String baseUri = "http://restapi.wcaquino.me:80/ola";
+
+    @Test
+    public void testHelloWorld() {
+        Response response = RestAssured.request(Method.GET, baseUri);
+
+        //O assertTrue deverá atender a uma condição ou uma lógica
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertTrue(response.statusCode() == 200);
+        Assert.assertTrue("Retorno esperado: 201, retorno obtido: "+response.statusCode(), response.statusCode()==201);
+
+        /*O assertEquals atende a uma comparação e retorna uma mensagem mais amigável
+        java.lang.AssertionError:
+        Expected :201
+        Actual   :200*/
+        Assert.assertEquals(201, response.statusCode());
+
+        //Uma outra forma de validar.
+        ValidatableResponse validation = response.then();
+        validation.statusCode(200);
+    }
+
+    @Test
+    public void umaFormaAbreviadaDeValidarRequisicoes() {
+        //Aqui pode-se resumir toda a cadeia anterior em poucas linhas, através do import static io.restassured.RestAssured.*;
+        get(baseUri).then().statusCode(200);
+    }
+
+    @Test
+    public void formaPadronizadaDeUtilizacaoRestAssured() {
+
+        given()
+            //Pré Condições
+        .when()
+            .get(baseUri)
+        .then()
+            .statusCode(200);
+    }
+}
---
+++ src/test/java/app/JunitHamCrestMatchers_v4_12.java
@@ -0,0 +1,61 @@
+//JUNIT version 4.12
+package app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.hamcrest.Matchers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+
+
+public class JunitHamCrestMatchers_v4_12 {
+
+    String baseUri = "http://restapi.wcaquino.me:80/ola";
+
+    @Test
+    public void testeHamcrestMatchers() {
+
+        Assert.assertThat("Maria", Matchers.is("Maria"));
+        Assert.assertThat("Maria", Matchers.is(not("Julia")));
+        Assert.assertThat(128, Matchers.is(128));
+        Assert.assertThat(128, Matchers.isA(Integer.class));
+        Assert.assertThat(128d, Matchers.isA(Double.class));
+        Assert.assertThat(128d, Matchers.greaterThan(120d));
+        Assert.assertThat(128d, Matchers.lessThan(130d));
+
+        List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9);
+        //Agora, depois de importar a lib Assert , Matchers
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1, 3, 5, 7, 9));
+        assertThat(impares, containsInAnyOrder(5, 3, 7, 9, 1));
+        assertThat(impares, hasItem(1));
+        assertThat(impares, hasItems(1, 5));
+
+        assertThat("Maria", is(not("Joao")));
+        assertThat("Maria", not("Julia"));
+        assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
+        assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
+    }
+
+    @Test
+    public void validandoBody() {
+
+        given()
+                //Pré Condições
+        .when()
+            .get(baseUri)
+        .then()
+            .statusCode(200)
+//            .body(Matchers.is("Ola Mundo!"));
+            .body(is("Ola Mundo!"));
+    }
+}
